c6c9a395e0eb1a8acc75fb8b3ec3532ee4683fa2
ColvTZzi <857008626@qq.com>
2024-06-08 10:57:35 +0800

    Merge Iterator done

--- mini-lsm-starter/src/iterators/merge_iterator.rs
+++ mini-lsm-starter/src/iterators/merge_iterator.rs
@@ -2,9 +2,15 @@
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
 use std::cmp::{self};
-use std::collections::BinaryHeap;
+use std::collections::binary_heap::PeekMut;
+use std::collections::{binary_heap, BinaryHeap};
+use std::iter;
+use std::ops::DerefMut;
+use std::sync::Arc;
+use std::thread::current;
 
-use anyhow::Result;
+use anyhow::{Ok, Result};
+use nom::character::complete::hex_digit0;
 
 use crate::key::KeySlice;
 
@@ -47,7 +53,18 @@ pub struct MergeIterator<I: StorageIterator> {
 
 impl<I: StorageIterator> MergeIterator<I> {
     pub fn create(iters: Vec<Box<I>>) -> Self {
-        unimplemented!()
+        let mut binary_heap: BinaryHeap<HeapWrapper<I>> = BinaryHeap::new();
+        for (index, iter) in iters.into_iter().enumerate() {
+            if iter.is_valid() {
+                binary_heap.push(HeapWrapper(index, iter));
+            }
+        }
+
+        let current = binary_heap.pop();
+        MergeIterator {
+            iters: binary_heap,
+            current,
+        }
     }
 }
 
@@ -57,18 +74,52 @@ impl<I: 'static + for<'a> StorageIterator<KeyType<'a> = KeySlice<'a>>> StorageIt
     type KeyType<'a> = KeySlice<'a>;
 
     fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.current.as_ref().unwrap().1.key()
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        self.current.as_ref().unwrap().1.value()
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.current.is_some()
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        // if other iter has the same key as current, invoke next()
+        while let Some(mut iter) = self.iters.peek_mut() {
+            if iter.1.key() == self.current.as_ref().unwrap().1.key() {
+                match iter.1.next() {
+                    Err(err) => {
+                        PeekMut::pop(iter);
+                        return Err(err);
+                    }
+                    _ => (),
+                }
+                if !iter.1.is_valid() {
+                    PeekMut::pop(iter);
+                }
+            } else {
+                break;
+            }
+        }
+
+        // current iter invoke next() and bring back to the heap
+        self.current.as_mut().unwrap().1.next()?;
+        let current = self.current.take();
+        if current.as_ref().unwrap().1.is_valid() {
+            self.iters.push(current.unwrap());
+        }
+
+        // peek until valid
+        while !self.iters.is_empty() && !self.iters.peek().unwrap().1.is_valid() {
+            self.iters.pop();
+        }
+
+        if !self.iters.is_empty() {
+            self.current = self.iters.pop();
+        }
+
+        Ok(())
     }
 }
